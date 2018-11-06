14c5524f8c400abab0c6e81633e0ea77ae42c9ee
Carolyn <ccpianist@gmail.com>
2018-11-05 21:18:14 -0800

    completed bubblesort

---
+++ bubblesort.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function bubbleSort(array){
+    // for(let i = 0; i < array.length; i++) { // passes
+    //     for (let j = 0; j < array.length; j++) { // bubbling
+
+    //         if (!inOrder(array, j)) swap(array, j);
+    //     }
+    // }
+
+    let sorted = false;
+    for (let end = array.length - 1; end >= 0 && !sorted; end--) { // passes
+        sorted = true;
+        for (let j = 0; j < end; j++) { // bubbling
+            if (!inOrder(array, j)) {
+                swap(array, j);
+                sorted = false;
+            }
+        }
+    }
+    return array;
+}
+
+function inOrder(arr, index) { // pure function
+    if (index === arr.length - 1) return true;
+    return arr[index] < arr[index + 1];
+}
+
+function swap (arr, index) { // side effects 
+    let oldLeft = arr[index];
+    arr[index] = arr[index + 1];
+    arr[index + 1] = oldLeft;
+}
+
+console.log(bubbleSort([3, 5, 1]));
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,3 +1,14 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-loop-func */
+
+function generageArray(size, lower, upper) {
+    let randomArray = [];
+    while (size--) {
+        let randomNum = Math.floor(lower + Math.random() * upper);
+        randomArray.push(randomNum);
+    } return randomArray;
+}
+
 describe('Bubble Sort', function(){
 
     it('sorts an empty array', function(){
@@ -8,7 +19,38 @@ describe('Bubble Sort', function(){
         expect(bubbleSort([8])).toEqual([8]);
     });
 
-    it('sorts an array of many elements', function(){
-        expect(bubbleSort([5,2,7,8,4,1,3,6])).toEqual([1,2,3,4,5,6,7,8]);
+    // it('sorts an array of many elements', function(){
+    //     expect(bubbleSort([5,2,7,8,4,1,3,6])).toEqual([1,2,3,4,5,6,7,8]);
+    // });
+
+    for (let i = 2; i < 103; i += 20) {
+        it('sorts an array of ' + i + 'random items', function(){
+            let arr = generageArray(i, 0, 100);
+            let sorted = arr.slice(0).sort((a,b) => a - b);
+            expect(bubbleSort(arr)).toEqual(sorted);
+        });
+    }
+
+    it('compares the expected number of times', function(){
+        spyOn(window, 'inOrder').and.callThrough();
+        bubbleSort([4,6,5,1]);
+        expect(inOrder.calls.count()).toEqual(10);
     });
-})
+
+    it('swaps the expected number of times', function(){
+        spyOn(window, 'swap').and.callThrough();
+        bubbleSort([4,6,5,1]);
+        expect(swap.calls.count()).toEqual(4);
+    });
+
+    // function spyOn (obj, method) {
+    //     let counter = 0;
+    //     let spy = function() {
+    //         count++;
+    //     }
+    //     obj[method] = spy;
+    //     spy.calls = {
+    //         count: function() { return counter; }
+    //     };
+    // }
+});
